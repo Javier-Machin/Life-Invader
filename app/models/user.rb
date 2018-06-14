9036339027439ea66b97350225211cdeabac2beb
Javier Machin <javier_machin_acosta@hotmail.com>
2018-06-14 12:42:24 +0100

    Add friendship model and user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  has_many :friendships, class_name: "Friendship", foreign_key: 'sender_id'
+  has_many :friends, through: :friendships, source: "receiver"
 end
