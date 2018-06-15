1d24cbfe8b3e04733c2099a8ef19a20460982872
Javier Machin <javier_machin_acosta@hotmail.com>
2018-06-15 18:40:42 +0100

    Add friend request tests

--- test/models/friend_request_test.rb
+++ test/models/friend_request_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class FriendRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @friendrequest = FriendRequest.new(sender: users(:user1),
+                                       receiver: users(:user2))
+  end
+
+  test "should be valid" do
+    assert @friendrequest.valid?
+  end
+
+  test "should require a sender" do
+    @friendrequest.sender = nil
+    assert_not @friendrequest.valid?
+  end
+
+  test "should require a receiver" do
+    @friendrequest.receiver = nil
+    assert_not @friendrequest.valid?
+  end
+
 end
