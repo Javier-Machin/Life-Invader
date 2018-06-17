b45b12315e57b920d8707e6b26aac56d716335a0
Javier Machin <javier_machin_acosta@hotmail.com>
2018-06-17 15:34:52 +0100

    Add user feed prototype and tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @posts = current_user.friends.posts.order(:created_at)
+    posts = []
+    current_user.friends.each do |friend|
+      posts << friend.posts.all.ids
+    end
+    posts << current_user.posts.all.ids
+    @posts = Post.order(:created_at).find(posts)
   end
+  
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
+
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "should redirect to sign in if not signed in" do
+    get root_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get index if signed in" do
+    @user = users(:user1)
+    @user.confirm
+    sign_in @user
+    get root_path
+    assert_select "div.Feed_container"
+  end
+
 end
