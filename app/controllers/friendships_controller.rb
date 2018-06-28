20d57914bc29d79bbecf449655e1cdc59aa283ee
Javier Machin <javier_machin_acosta@hotmail.com>
2018-06-28 20:18:07 +0100

    Add unfriend functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,5 +5,11 @@ class FriendshipsController < ApplicationController
     current_user.friendships.create(receiver: User.find(params[:id]))
     redirect_back(fallback_location: root_path)
   end
-
+  
+  def destroy
+    @friendship = Friendship.find_by(sender: current_user, receiver: User.find(params[:id]))
+    @friendship.destroy
+    redirect_back(fallback_location: root_path)
+  end
+  
 end
