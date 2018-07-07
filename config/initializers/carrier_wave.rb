9e62435e6e23cd229b91330626d8f9d0a7f61034
Javier Machin <javier_machin_acosta@hotmail.com>
2018-07-07 18:31:12 +0100

    Setting up deployment

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,14 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Configuration for Amazon S3
+
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
