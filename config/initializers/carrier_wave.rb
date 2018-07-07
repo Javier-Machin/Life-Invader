4a245eba1533eaf2f908a9ea3a177e27bed1f369
Javier Machin <javier_machin_acosta@hotmail.com>
2018-07-07 19:16:26 +0100

    Setting up deployment

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -11,7 +11,7 @@ if Rails.env.production?
 
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       :region                => 'eu-west-2' 
     }
     config.fog_directory     =  ENV['S3_BUCKET']
