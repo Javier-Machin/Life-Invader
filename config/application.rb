ef6778a28e6c8f0d30f650d655404c8ca3db372e
Javier Machin <13190712+Javier-Machin@users.noreply.github.com>
2023-10-20 23:01:48 +0100

    chore: update to rails 7.1.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module LifeInvader
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.1
+    config.active_support.cache_format_version = 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_01_163434) do
-
+ActiveRecord::Schema[7.1].define(version: 2018_07_01_163434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.integer "author_id"
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "post_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -28,8 +27,8 @@ ActiveRecord::Schema.define(version: 2018_07_01_163434) do
   create_table "friend_requests", force: :cascade do |t|
     t.integer "request_sender"
     t.integer "request_receiver"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["request_receiver"], name: "index_friend_requests_on_request_receiver"
     t.index ["request_sender"], name: "index_friend_requests_on_request_sender"
   end
@@ -37,8 +36,8 @@ ActiveRecord::Schema.define(version: 2018_07_01_163434) do
   create_table "friendships", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "receiver_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["receiver_id"], name: "index_friendships_on_receiver_id"
     t.index ["sender_id"], name: "index_friendships_on_sender_id"
     t.index ["sender_id"], name: "index_friendships_on_sender_id_and_sender_id"
@@ -49,8 +48,8 @@ ActiveRecord::Schema.define(version: 2018_07_01_163434) do
     t.integer "post_id"
     t.integer "likeable_id"
     t.string "likeable_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["likeable_id", "likeable_type"], name: "index_likes_on_likeable_id_and_likeable_type"
   end
@@ -58,8 +57,8 @@ ActiveRecord::Schema.define(version: 2018_07_01_163434) do
   create_table "posts", force: :cascade do |t|
     t.integer "author_id"
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "picture"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
@@ -68,21 +67,21 @@ ActiveRecord::Schema.define(version: 2018_07_01_163434) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
     t.string "name"
     t.date "birthdate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "profile"
     t.string "provider"
     t.string "uid"
