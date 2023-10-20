2af232db328eccec037f63c40c17a5c0be28d159
Javier M <13190712+Javier-Machin@users.noreply.github.com>
2023-10-20 22:00:45 +0100

    chore: update to ruby 3, update to rails 6.1 (#2)

--- db/migrate/20180613103411_devise_create_users.rb
+++ db/migrate/20180613103411_devise_create_users.rb
@@ -33,7 +33,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :name
-      t.string :email
       t.date :birthdate
 
       t.timestamps null: false
